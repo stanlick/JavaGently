0cca8dfe1542fa6154203c6ca3f0f27038b5d37c
J Scott Stanlick <stanlick@gmail.com>
2019-07-16 09:32:50 -0500

    add js file

---
+++ src/test.js
@@ -0,0 +1,21 @@
+// function sum(a, b, ...args){
+//     let sum = 0;
+//     for(let arg of args) sum+= arg;
+//     return sum;
+// }
+
+// show(   sum(2,2,2)   );
+
+
+function show(...params) {
+    console.log(params);
+}
+
+
+const arr=['said', 20, 'JavaScript enthusiast', 'Hi', 'Said', 'How are you?'];
+const arr2=[v1,v2,   ...rest]=arr
+
+show( arr2 );
+show( rest );
+
+
