4f3b4250a205b0bf9690390ba133aa24e76d38cd
Scott Stanlick <stanlick@gmail.com>
2019-07-18 10:22:17 -0500

    Update thermometer.js

--- src/thermometer.js
+++ src/thermometer.js
@@ -6,7 +6,7 @@ function toCels(fahr){
     return ((fahr - 32) *5/9).toFixed(2);
 }
 
-show("80F is", toCels(80), "C");     //26.67 °C
+show("80 is", toCels(80), "C");     //26.67 °C
 show("26.67C is", toFahr(26.67), "F");   //80 °F
 
 function show(...s){
